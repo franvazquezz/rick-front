a20752eca9a44923588db9f0b446af32f9de0fa4
franvazquezz <franvazquez2001@gmail.com>
2023-06-28 15:46:28 -0300

    show

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './Redux/store'
-import axios from 'axios'
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './Redux/store';
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://rick-back-production-dd5d.up.railway.app/rickandmorty';
 
 
-ReactDOM.render(
+createRoot(document.getElementById('root'))
+  .render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <App/>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
-)
+);
